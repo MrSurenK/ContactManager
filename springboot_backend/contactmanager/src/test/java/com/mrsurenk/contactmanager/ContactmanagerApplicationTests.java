b30a6329c9b29ef75f0a08ee15c098db24280c24
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-03-31 16:50:52 +0800

    initialise springboot app

---
+++ springboot_backend/contactmanager/src/test/java/com/mrsurenk/contactmanager/ContactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrsurenk.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
