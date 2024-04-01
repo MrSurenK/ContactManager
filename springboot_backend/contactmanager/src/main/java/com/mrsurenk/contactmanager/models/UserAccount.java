d8e319ca3e30b337d6085b99b350ed8b3cf700a0
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-04-01 16:29:31 +0800

    Created account entity

--- springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/models/UserAccount.java
+++ springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/models/UserAccount.java
@@ -1,7 +1,28 @@
 package com.mrsurenk.contactmanager.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 @Entity
 public class UserAccount {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String userName;
+
+    private String password;
+
+    private String contact;
+
+    private Date lastLogin;
+
+    private boolean hidden;
+
 }
