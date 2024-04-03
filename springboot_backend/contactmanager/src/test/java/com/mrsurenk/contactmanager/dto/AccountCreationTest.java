c7cd8f3e0a3c009fd9b3701f353b840ad87366f3
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-04-03 21:02:47 +0800

    Tested AccountCreation DTO validation successfully

--- springboot_backend/contactmanager/src/test/java/com/mrsurenk/contactmanager/dto/AccountCreationTest.java
+++ springboot_backend/contactmanager/src/test/java/com/mrsurenk/contactmanager/dto/AccountCreationTest.java
@@ -1,4 +1,63 @@
 package com.mrsurenk.contactmanager.dto;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class AccountCreationTest {
+
+    //Test valid account creation
+    @Test
+    public void testValidAccountCreation(){
+        assertDoesNotThrow(()-> new AccountCreation(
+                "test@example.com",
+                "strongpassword",
+                "john",
+                "+6581234565"
+        ));
+    }
+
+    //Test validations
+    @Test
+    public void testInvalidEmail(){
+        assertThrows(IllegalArgumentException.class, ()-> new AccountCreation(
+                "invalidemail",
+                "strongpassword",
+                "John",
+                "+6581234565"
+        ));
+    }
+
+    @Test
+    public void testInvalidPassword(){
+        assertThrows(IllegalArgumentException.class, ()-> new AccountCreation(
+                "test@example.com",
+                "invalid",
+                "John",
+                "+6581234565"
+        ));
+    }
+
+    @Test
+    public void testInvalidName(){
+        assertThrows(IllegalArgumentException.class, ()-> new AccountCreation(
+                "test@example.com",
+                "invalid",
+                "",
+                "+6581234565"
+        ));
+    }
+
+    @Test
+    public void testInvalidSGContact(){
+        assertThrows(IllegalArgumentException.class, ()-> new AccountCreation(
+                 "test@example.com",
+                "invalid",
+                "",
+                "+12234565"
+        ));
+    }
 }
