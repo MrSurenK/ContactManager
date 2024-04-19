80cd770a5def5456c25649eb22e32c1555cc285b
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-04-19 17:39:47 +0800

    setting up jwt with spring security

---
+++ springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/configurations/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.mrsurenk.contactmanager.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+}
--- springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/services/ImageUploadService.java
+++ springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/services/ImageUploadService.java
@@ -1,5 +1,6 @@
 package com.mrsurenk.contactmanager.services;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 @Service
+@Log
 public class ImageUploadService {
 
 
@@ -14,8 +16,10 @@ public class ImageUploadService {
         //ToDo: Change to relative path before deploying(This path is only for local dev)
         String FOLDER_PATH = "/Users/suren/Projects/ContactManager/springboot_backend/contactmanager/src/main/resources/displaypics/";
         if (image == null || image.isEmpty()) {
+//            log.info("Default image only");
             return FOLDER_PATH + "default_dp.png";
         } else {
+//            log.info("Getting the uploaded image");
             String filePath = FOLDER_PATH + image.getOriginalFilename();
             File destination = new File(filePath);
             image.transferTo(destination);
