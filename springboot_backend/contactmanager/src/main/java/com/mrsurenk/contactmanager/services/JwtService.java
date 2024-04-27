c7bdc9c52f509c05fe978dd6a0bd640b70e28dff
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-04-27 16:17:51 +0800

    Reference link in jwtService

--- springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/services/JwtService.java
+++ springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/services/JwtService.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+//reference: https://medium.com/@tericcabrel/implement-jwt-authentication-in-a-spring-boot-3-application-5839e4fd8fac
 @Service
 public class JwtService {
     @Value("${security.jwt.secret-key}")
