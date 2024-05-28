49a458f37683c62b3f851aab335f4b90ba311033
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-05-28 17:54:59 +0800

    Set up registration skeleton

---
+++ vue_frontend/contact-manager/src/components/RegistrationForm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+const contact = ref('')
+</script>
+
+<template>
+  <p>Already have an account?</p>
+  <nav>
+    <RouterLink to="/">Sign in</RouterLink>
+  </nav>
+  <form @submit.prevent>
+    <label>Name</label>
+    <input v-model="name" />
+    <label>Email</label>
+    <input v-model="email" />
+    <label>Password</label>
+    <input v-model="password" type="password" />
+    <label>Contact No</label>
+    <input v-model="contact" type="tel" placeholder="+65 88612345" />
+    <button type="submit">Register</button>
+  </form>
+</template>
+
+<style scoped></style>
--- vue_frontend/contact-manager/src/views/Register.vue
+++ vue_frontend/contact-manager/src/views/Register.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RegistrationForm from '../components/RegistrationForm.vue'
+</script>
 
 <template>
   <h1>Registration Page</h1>
+  <RegistrationForm />
 </template>
