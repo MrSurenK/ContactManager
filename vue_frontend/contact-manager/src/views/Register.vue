f5d8b908f6cf5f9c4936e6a65d6e1fc41eff032a
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-05-31 11:11:30 +0800

    Registration page Css done

--- vue_frontend/contact-manager/src/views/Register.vue
+++ vue_frontend/contact-manager/src/views/Register.vue
@@ -11,4 +11,27 @@ import RegistrationForm from '../components/RegistrationForm.vue'
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.register-page {
+  min-height: 100vh;
+  background-color: #89cff0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+#register-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2em;
+  border: solid 2px black;
+  padding: 5em 8em 5em 8em;
+  box-shadow:
+    8px 8px 8px 0 rgba(0, 0, 0, 0.2),
+    10px 10px 20px 0 rgba(0, 0, 0, 0.19);
+  background-color: rgba(23, 45, 38, 0.2);
+}
+</style>
