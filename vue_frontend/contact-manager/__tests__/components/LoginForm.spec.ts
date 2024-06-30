ab45ae08357b235663ca3249e4cf23118c57f3e6
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-06-30 18:11:42 +0800

    Defining 1st front end test case

---
+++ vue_frontend/contact-manager/__tests__/components/LoginForm.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+test('should render dashboard when logged in', () => {
+  expect()
+})
+
+// Write a test to test if dashboard is rendered when logged in successfully
