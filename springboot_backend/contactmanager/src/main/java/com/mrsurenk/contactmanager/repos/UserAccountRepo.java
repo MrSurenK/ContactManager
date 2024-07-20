12fc22b0cd442f4ad8f55dea592867dbb9906126
MrSurenK <surenkworkz@gmail.com>
2024-07-20 15:23:59 +0800

    Writing search function

--- springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/repos/UserAccountRepo.java
+++ springboot_backend/contactmanager/src/main/java/com/mrsurenk/contactmanager/repos/UserAccountRepo.java
@@ -3,9 +3,13 @@ package com.mrsurenk.contactmanager.repos;
 import com.mrsurenk.contactmanager.models.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserAccountRepo extends CrudRepository<UserAccount, UUID> {
     Optional<UserAccount> findByEmail(String email);
+    List<UserAccount> findByNameContainingIgnoreCase(String name);
+
+
 }
--- springboot_backend/contactmanager/src/test/java/com/mrsurenk/contactmanager/services/FriendServiceTest.java
+++ springboot_backend/contactmanager/src/test/java/com/mrsurenk/contactmanager/services/FriendServiceTest.java
@@ -1,5 +1,7 @@
 package com.mrsurenk.contactmanager.services;
 
+import com.mrsurenk.contactmanager.models.UserAccount;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +10,16 @@ public class FriendServiceTest {
     //TDD principles
 
     //Instantiate two users for testing purposes
+    @Before
+    public void friendsSetUp(){
 
 
+    }
 
     // Method 1: Send friend request
     @Test
     @DisplayName("Test if user can find friend")
-    public void searchTest(){
+    public void searchTest(UserAccount friend){
 
 
     }
